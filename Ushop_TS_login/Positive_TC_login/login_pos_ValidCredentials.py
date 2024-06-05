6a19ae85b0914a7a77bab0677d2e1ae6cbd4b4b7
ayomiudeshika <ayomiudeshika19@gmail.com>
2024-06-05 17:11:50 +0530

    third commit

--- Ushop_TS_login/Positive_TC_login/login_pos_ValidCredentials.py
+++ Ushop_TS_login/Positive_TC_login/login_pos_ValidCredentials.py
@@ -5,27 +5,22 @@ import time
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.implicitly_wait(10)
 
 def login():
     OTP="9774"
     driver.get("https://uat.ushop.lk/")
     driver.find_element(By.XPATH, '//*[@id="body"]/body/div[1]/div/div[1]/div[1]/div[1]/div/button').click()
-    time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="phone-no"]').send_keys("767778194")
-    time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="body"]/body/div[1]/div/div[2]/form/div/button').click()
     time.sleep(20)
     driver.find_element(By.XPATH, '//*[@id="phone-no"]').send_keys("OTP")
-    time.sleep(10)
     driver.find_element(By.XPATH, '//*[@id="body"]/body/div[1]/div/div[2]/form/div/button').click()
-    time.sleep(2)
-    driver.find_element(By.XPATH, '//*[@id="body"]/body/div[1]/div/div[2]/form/div[2]/button').click()
     time.sleep(2)
     # otp_field = driver.find_element(By.ID, "otp_field_id")
     # otp_field.send_keys(otp)
 
 login()
-time.sleep(2)
 driver.close()
 
 #PASS
--- Ushop_TS_orders/Positive_TC_orders/orders_view.py
+++ Ushop_TS_orders/Positive_TC_orders/orders_view.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.implicitly_wait(10)
@@ -16,14 +16,15 @@ def login():
     driver.find_element(By.XPATH, '//*[@id="body"]/body/div[1]/div/div[1]/div[1]/div[1]/div/button').click()
     driver.find_element(By.XPATH, '//*[@id="phone-no"]').send_keys("767778194")
     driver.find_element(By.XPATH, '//*[@id="body"]/body/div[1]/div/div[2]/form/div/button').click()
-    driver.find_element(By.XPATH, '//*[@id="phone-no"]').send_keys("OTP")
     time.sleep(20)
+    driver.find_element(By.XPATH, '//*[@id="phone-no"]').send_keys("OTP")
     driver.find_element(By.XPATH, '//*[@id="body"]/body/div[1]/div/div[2]/form/div/button').click()
+    time.sleep(5)
 
 
 def view_orders():
     driver.find_element(By.XPATH,'//*[@id="body"]/body/nav[1]/div[1]/div/div[3]/div[1]/a[1]').click()
-    time.sleep(5)
+    time.sleep(2)
 
 
 login()
